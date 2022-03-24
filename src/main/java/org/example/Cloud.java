f4fa4496061f03d2ee684f5bcde8a7c9fe095cd8
giuseppe <giuseppe.anzillotti@mail.polimi.it>
2022-03-24 15:33:05 +0100

    Cloud Class

---
+++ src/main/java/org/example/Cloud.java
@@ -0,0 +1,25 @@
+package src.main.java.org.example;
+
+import java.awt.image.AreaAveragingScaleFilter;
+import java.util.ArrayList;
+
+public class Cloud {
+    private ArrayList<Student> students = new ArrayList<>();
+    private boolean choosen;
+
+    public ArrayList<Student> getStudents() {
+        return students;
+    }
+
+    public boolean isChoosen() {
+        return choosen;
+    }
+
+    public void setChoosen(boolean choosen) {
+        this.choosen = choosen;
+    }
+
+    public void setStudents(ArrayList<Student> students) {
+        this.students = students;
+    }
+}
