0de843e68cff6ac3271b5fe0463875a080ee4aad
samub <samuele.borgonovo@mail.polimi.it>
2022-04-03 16:40:14 +0200

    update

--- src/main/java/it/polimi/ingsw/model/Plance.java
+++ src/main/java/it/polimi/ingsw/model/Plance.java
@@ -7,15 +7,13 @@ import java.util.ArrayList;
 public class Plance {
     private ArrayList<Student> entrance = new ArrayList<>();
     private ArrayList<Professor> tableProfessor = new ArrayList<>();
-    private Student[][] hall = new Student[5][5];
+    private final Student[][] hall = new Student[5][10];
     private ArrayList<Tower> towers = new ArrayList<>();
 
-    public Plance(ArrayList<Student> entrance, ArrayList<Professor> tableProfessor, Student[][] hall, ArrayList<Tower> towers) {
+    public Plance(ArrayList<Student> entrance, ArrayList<Professor> tableProfessor, ArrayList<Tower> towers) {
         this.entrance = entrance;
         this.tableProfessor = tableProfessor;
-        this.hall = hall;
         this.towers = towers;
-
     }
 
     public ArrayList<Student> getEntrance() {
@@ -26,59 +24,71 @@ public class Plance {
         return tableProfessor;
     }
 
-    public int getNumOfTowers(){
+    public int getNumOfTowers() {
         return towers.size();
     }
 
-    public void addProfessor(Professor professor){
+    public void addProfessor(Professor professor) {
         tableProfessor.add(professor);
     }
 
-    public void removeProfessor(Professor professor){
+    public void removeProfessor(Professor professor) {
         tableProfessor.remove(professor);
 
     }
 
-    public void addTower(Tower tower){
+    public void addTower(Tower tower) {
         towers.add(tower);
     }
 
-    public void removeTower(){
-        towers.remove(towers.size()-1);
+    public void removeTower() {
+        towers.remove(towers.size() - 1);
     }
 
-    public void addStudentEntrance(Student student){
+    public void addStudentEntrance(Student student) {
         entrance.add(student);
     }
 
-    public void removeStudentEntrance(Student student){
+    public void removeStudentEntrance(Student student) {
         entrance.remove(student);
     }
 
-    public void addStudentHall(@NotNull Student student){
-       // hall.get(student.ordinal()).add(student);
+    public void addStudentHall(@NotNull Student student) {
+        // hall.get(student.ordinal()).add(student);
         //ArrayList<Student> s = new ArrayList<>();
         //s.add(student);
         //hall.add(student.ordinal(),s);
-        for(int i=0;i<10;i++)
-            if(hall[student.ordinal()][i] == null)
+        for (int i = 0; i < 10; i++)
+            if (hall[student.ordinal()][i] == null) {
                 hall[student.ordinal()][i] = student;
-
+                i = 10;
+            }
     }
 
-    public Student[][] getStudentHall(){
+    public Student[][] getStudentHall() {
         return hall;
     }
 
-    public Student removeStudentFromHall(Student student){
-        int i=0;
-        if(hall[student.ordinal()][0]==null)
+    public Student removeStudentFromHall(@NotNull Student student) {
+        if (hall[student.ordinal()][0] == null)
             return null;
-        else
-            while(hall[student.ordinal()][i+1]!=null)
-                i++;
-            hall[student.ordinal()][i]=null;
+        else {
+            for (int i = 0; i < 10; i++)
+                if (i == 9) {
+                    hall[student.ordinal()][i] = null;
+                    break;
+                } else if (hall[student.ordinal()][i + 1] == null) {
+                    hall[student.ordinal()][i] = null;
+                    break;
+                }
             return student;
+        }
+        //while(hall[student.ordinal()][i+1]!=null && i!=9) {
+        //    i++;
+
+        //}
+        //  hall[student.ordinal()][i]=null;
+        // return student;
     }
 
 }
--- src/test/java/it/polimi/ingsw/TestPlance.java
+++ src/test/java/it/polimi/ingsw/TestPlance.java
@@ -11,37 +11,283 @@ public class TestPlance {
         @Test
         public void testAddStudentToHall01()
         {
-            ArrayList<ArrayList<Student>> hallWanted = new ArrayList<>();
+            Student[][] hallWanted = {
+                    {Student.GREEN,Student.GREEN,null,null,null,null,null,null,null,null},
+                    {Student.RED,null,null,null,null,null,null,null,null,null},
+                    {Student.YELLOW,null,null,null,null,null,null,null,null,null},
+                    {Student.PINK,Student.PINK,Student.PINK,null,null,null,null,null,null,null},
+                    {Student.BLUE,Student.BLUE,null,null,null,null,null,null,null,null}
+            };
 
-            ArrayList<Student> entrance2 = new ArrayList<>();
-            ArrayList<Professor> tableProfessor2 = new ArrayList<>();
-            ArrayList<ArrayList<Student>> hall2 = new ArrayList<>();
-            ArrayList<Tower> towers2 = new ArrayList<>();
-
-            String nickname= "LUCA";
-            int playerID=0;
-            PlayerState playerState= PlayerState.PLAYINGYOURTURN;
-            Plance plance= new Plance(entrance2, tableProfessor2,hall2,towers2);
-            final Wizard wizard= Wizard.WIZARDBLUE;
-            int coins=0;
-            ArrayList<Assistant> assistantCards = new ArrayList<>();
 
             ArrayList<Student> entrance = new ArrayList<>();
             ArrayList<Professor> tableProfessor = new ArrayList<>();
-            ArrayList<ArrayList<Student>> hall = new ArrayList<>();
+            Student [][] hall;
+            hall = new Student[5][];
+            hall[0] = new Student[10];
+            hall[1] = new Student[10];
+            hall[2] = new Student[10];
+            hall[3] = new Student[10];
+            hall[4] = new Student[10];
             ArrayList<Tower> towers = new ArrayList<>();
-            Player player= new Player(nickname,playerID,playerState,plance,wizard,coins,assistantCards);
 
-            Plance p = new Plance(entrance, tableProfessor,hall,towers);
-            hallWanted.get(0).add(Student.GREEN);
-            hallWanted.get(0).add(Student.GREEN);
-            hallWanted.get(1).add(Student.RED);
-            hallWanted.get(3).add(Student.PINK);
-            hallWanted.get(3).add(Student.PINK);
-            hallWanted.get(3).add(Student.PINK);
-            hallWanted.get(4).add(Student.BLUE);
+
+            Plance p = new Plance(entrance, tableProfessor,towers);
+
+            p.addStudentHall(Student.PINK);
             p.addStudentHall(Student.GREEN);
+            p.addStudentHall(Student.GREEN);
+            p.addStudentHall(Student.RED);
+            p.addStudentHall(Student.YELLOW);
+            p.addStudentHall(Student.PINK);
+            p.addStudentHall(Student.PINK);
+            p.addStudentHall(Student.BLUE);
+            p.addStudentHall(Student.BLUE);
 
+            hall= p.getStudentHall();
+            int k=0;
+            for(int i=0;i<5;i++)
+                for(int j=0; j<10;j++)
+                    if (hall[i][j] != hallWanted[i][j]) {
+                        k = -1;
+                        break;
+                    }
+            assertEquals(k,0);
         }
 
+    @Test
+    public void testRemoveStudent01()
+    {
+        Student[][] hallWanted = {
+                {Student.GREEN,Student.GREEN,null,null,null,null,null,null,null,null},
+                {Student.RED,null,null,null,null,null,null,null,null,null},
+                {Student.YELLOW,null,null,null,null,null,null,null,null,null},
+                {Student.PINK,Student.PINK,null,null,null,null,null,null,null,null},
+                {Student.BLUE,Student.BLUE,null,null,null,null,null,null,null,null}
+        };
+
+
+        ArrayList<Student> entrance = new ArrayList<>();
+        ArrayList<Professor> tableProfessor = new ArrayList<>();
+        Student [][] hall;
+        hall = new Student[5][];
+        hall[0] = new Student[10];
+        hall[1] = new Student[10];
+        hall[2] = new Student[10];
+        hall[3] = new Student[10];
+        hall[4] = new Student[10];
+        ArrayList<Tower> towers = new ArrayList<>();
+
+
+        Plance p = new Plance(entrance, tableProfessor,towers);
+
+        p.addStudentHall(Student.PINK);
+        p.addStudentHall(Student.GREEN);
+        p.addStudentHall(Student.GREEN);
+        p.addStudentHall(Student.RED);
+        p.addStudentHall(Student.YELLOW);
+        p.addStudentHall(Student.PINK);
+        p.addStudentHall(Student.PINK);
+        p.addStudentHall(Student.BLUE);
+        p.addStudentHall(Student.BLUE);
+
+        p.removeStudentFromHall(Student.PINK);
+
+        hall= p.getStudentHall();
+      //  int k=0;
+        //  for(int i=0;i<5;i++)
+      //      for(int j=0; j<10;j++)
+       //         if (hall[i][j] != hallWanted[i][j]) {
+       //             k = -1;
+        //            break;
+         //       }
+        //assertEquals(k,0);
+
+        for(int z=0;z<5;z++)
+            for(int l=0;l<10;l++)
+                System.out.println(hall[z][l]);
     }
+
+    @Test
+    public void testRemoveStudent02()
+    {
+        Student[][] hallWanted = {
+                {Student.GREEN,Student.GREEN,null,null,null,null,null,null,null,null},
+                {Student.RED,Student.RED,Student.RED,Student.RED,Student.RED,Student.RED,Student.RED,Student.RED,Student.RED,null},
+                {Student.YELLOW,null,null,null,null,null,null,null,null,null},
+                {Student.PINK,Student.PINK,null,null,null,null,null,null,null,null},
+                {Student.BLUE,Student.BLUE,null,null,null,null,null,null,null,null}
+        };
+
+
+        ArrayList<Student> entrance = new ArrayList<>();
+        ArrayList<Professor> tableProfessor = new ArrayList<>();
+        Student [][] hall;
+        hall = new Student[5][];
+        hall[0] = new Student[10];
+        hall[1] = new Student[10];
+        hall[2] = new Student[10];
+        hall[3] = new Student[10];
+        hall[4] = new Student[10];
+        ArrayList<Tower> towers = new ArrayList<>();
+
+
+        Plance p = new Plance(entrance, tableProfessor,towers);
+
+        p.addStudentHall(Student.GREEN);
+        p.addStudentHall(Student.GREEN);
+        p.addStudentHall(Student.RED);
+        p.addStudentHall(Student.RED);
+        p.addStudentHall(Student.RED);
+        p.addStudentHall(Student.RED);
+        p.addStudentHall(Student.RED);
+        p.addStudentHall(Student.RED);
+        p.addStudentHall(Student.RED);
+        p.addStudentHall(Student.RED);
+        p.addStudentHall(Student.RED);
+        p.addStudentHall(Student.RED);
+        p.addStudentHall(Student.YELLOW);
+        p.addStudentHall(Student.PINK);
+        p.addStudentHall(Student.PINK);
+        p.addStudentHall(Student.BLUE);
+        p.addStudentHall(Student.BLUE);
+
+        p.removeStudentFromHall(Student.RED);
+
+        hall= p.getStudentHall();
+        int k=0;
+        for(int i=0;i<5;i++)
+            for(int j=0; j<10;j++)
+                if (hall[i][j] != hallWanted[i][j]) {
+                    k = -1;
+                    break;
+                }
+        assertEquals(k,0);
+
+        for(int z=0;z<5;z++)
+            for(int l=0;l<10;l++)
+                System.out.println(hall[z][l]);
+    }
+
+
+    @Test
+    public void testRemoveStudent03()
+    {
+        Student[][] hallWanted = {
+                {Student.GREEN,Student.GREEN,null,null,null,null,null,null,null,null},
+                {Student.RED,Student.RED,Student.RED,Student.RED,Student.RED,Student.RED,Student.RED,Student.RED,Student.RED,null},
+                {null,null,null,null,null,null,null,null,null,null},
+                {Student.PINK,Student.PINK,null,null,null,null,null,null,null,null},
+                {Student.BLUE,Student.BLUE,null,null,null,null,null,null,null,null}
+        };
+
+
+        ArrayList<Student> entrance = new ArrayList<>();
+        ArrayList<Professor> tableProfessor = new ArrayList<>();
+        Student [][] hall;
+        hall = new Student[5][];
+        hall[0] = new Student[10];
+        hall[1] = new Student[10];
+        hall[2] = new Student[10];
+        hall[3] = new Student[10];
+        hall[4] = new Student[10];
+        ArrayList<Tower> towers = new ArrayList<>();
+
+
+        Plance p = new Plance(entrance, tableProfessor,towers);
+
+        p.addStudentHall(Student.GREEN);
+        p.addStudentHall(Student.GREEN);
+        p.addStudentHall(Student.RED);
+        p.addStudentHall(Student.RED);
+        p.addStudentHall(Student.RED);
+        p.addStudentHall(Student.RED);
+        p.addStudentHall(Student.RED);
+        p.addStudentHall(Student.RED);
+        p.addStudentHall(Student.RED);
+        p.addStudentHall(Student.RED);
+        p.addStudentHall(Student.RED);
+        p.addStudentHall(Student.PINK);
+        p.addStudentHall(Student.PINK);
+        p.addStudentHall(Student.BLUE);
+        p.addStudentHall(Student.BLUE);
+
+        p.removeStudentFromHall(Student.YELLOW);
+
+        hall= p.getStudentHall();
+        int k=0;
+        for(int i=0;i<5;i++)
+            for(int j=0; j<10;j++)
+                if (hall[i][j] != hallWanted[i][j]) {
+                    k = -1;
+                    break;
+                }
+        assertEquals(k,0);
+
+        for(int z=0;z<5;z++)
+            for(int l=0;l<10;l++)
+                System.out.println(hall[z][l]);
+    }
+
+    @Test
+    public void testRemoveStudent04()
+    {
+        Student[][] hallWanted = {
+                {Student.GREEN,Student.GREEN,null,null,null,null,null,null,null,null},
+                {Student.RED,Student.RED,Student.RED,Student.RED,Student.RED,Student.RED,Student.RED,Student.RED,Student.RED,null},
+                {null,null,null,null,null,null,null,null,null,null},
+                {Student.PINK,Student.PINK,null,null,null,null,null,null,null,null},
+                {Student.BLUE,Student.BLUE,null,null,null,null,null,null,null,null}
+        };
+
+
+        ArrayList<Student> entrance = new ArrayList<>();
+        ArrayList<Professor> tableProfessor = new ArrayList<>();
+        Student [][] hall;
+        hall = new Student[5][];
+        hall[0] = new Student[10];
+        hall[1] = new Student[10];
+        hall[2] = new Student[10];
+        hall[3] = new Student[10];
+        hall[4] = new Student[10];
+        ArrayList<Tower> towers = new ArrayList<>();
+
+
+        Plance p = new Plance(entrance, tableProfessor,towers);
+
+        p.addStudentHall(Student.GREEN);
+        p.addStudentHall(Student.GREEN);
+        p.addStudentHall(Student.RED);
+        p.addStudentHall(Student.RED);
+        p.addStudentHall(Student.RED);
+        p.addStudentHall(Student.RED);
+        p.addStudentHall(Student.RED);
+        p.addStudentHall(Student.RED);
+        p.addStudentHall(Student.RED);
+        p.addStudentHall(Student.RED);
+        p.addStudentHall(Student.RED);
+        p.addStudentHall(Student.YELLOW);
+        p.addStudentHall(Student.PINK);
+        p.addStudentHall(Student.PINK);
+        p.addStudentHall(Student.BLUE);
+        p.addStudentHall(Student.BLUE);
+
+        p.removeStudentFromHall(Student.YELLOW);
+        p.removeStudentFromHall(Student.YELLOW);
+
+        hall= p.getStudentHall();
+        int k=0;
+        for(int i=0;i<5;i++)
+            for(int j=0; j<10;j++)
+                if (hall[i][j] != hallWanted[i][j]) {
+                    k = -1;
+                    break;
+                }
+        assertEquals(k,0);
+
+        for(int z=0;z<5;z++)
+            for(int l=0;l<10;l++)
+                System.out.println(hall[z][l]);
+    }
+
+}
