267fc3253e56e881ef3dade3dff02a9b791973e3
samub <samuele.borgonovo@mail.polimi.it>
2022-05-26 22:08:56 +0200

    fix wizard controller

--- src/main/java/it/polimi/ingsw/client/View/gui/WizardController.java
+++ src/main/java/it/polimi/ingsw/client/View/gui/WizardController.java
@@ -34,33 +34,57 @@ public class WizardController {
     }
 
     public void visibleWizards(ArrayList<Wizard> wizards){
-        if(!wizards.contains(Wizard.WIZARD_GREEN))
-            wizardGreen.setVisible(false);
-        if(!wizards.contains(Wizard.WIZARD_YELLOW))
-            wizardYellow.setVisible(false);
-        if(!wizards.contains(Wizard.WIZARD_BLUE))
-            wizardBlue.setVisible(false);
-        if(!wizards.contains(Wizard.WIZARD_PINK))
-            wizardPink.setVisible(false);
+        if(!wizards.contains(Wizard.WIZARD_GREEN)){
+            wizardGreen.setDisable(true);
+            wizardGreen.setOpacity(0.3);
+        }
+        if(!wizards.contains(Wizard.WIZARD_YELLOW)) {
+            wizardYellow.setDisable(true);
+            wizardYellow.setOpacity(0.3);
+        }
+        if(!wizards.contains(Wizard.WIZARD_BLUE)) {
+            wizardBlue.setDisable(true);
+            wizardBlue.setOpacity(0.3);
+        }
+        if(!wizards.contains(Wizard.WIZARD_PINK)) {
+            wizardPink.setDisable(true);
+            wizardPink.setOpacity(0.3);
+        }
     }
 
     public void choosenWizardGreen(MouseEvent mouseEvent) {
         ChooseWizardMessage message = new ChooseWizardMessage(Wizard.WIZARD_GREEN);
         gui.getClient().sendMessage(message);
+        wizardYellow.setDisable(true);
+        wizardBlue.setDisable(true);
+        wizardPink.setDisable(true);
+        System.out.println("sciao");
     }
 
     public void choosenWizardYellow(MouseEvent mouseEvent) {
         ChooseWizardMessage message = new ChooseWizardMessage(Wizard.WIZARD_YELLOW);
         gui.getClient().sendMessage(message);
+        wizardGreen.setDisable(true);
+        wizardBlue.setDisable(true);
+        wizardPink.setDisable(true);
+        System.out.println("sciao");
     }
 
     public void choosenWizardBlue(MouseEvent mouseEvent) {
         ChooseWizardMessage message = new ChooseWizardMessage(Wizard.WIZARD_BLUE);
         gui.getClient().sendMessage(message);
+        wizardYellow.setDisable(true);
+        wizardGreen.setDisable(true);
+        wizardPink.setDisable(true);
+        System.out.println("sciao");
     }
 
     public void choosenWizardPink(MouseEvent mouseEvent) {
         ChooseWizardMessage message = new ChooseWizardMessage(Wizard.WIZARD_PINK);
         gui.getClient().sendMessage(message);
+        wizardYellow.setDisable(true);
+        wizardBlue.setDisable(true);
+        wizardGreen.setDisable(true);
+        System.out.println("sciao");
     }
 }
