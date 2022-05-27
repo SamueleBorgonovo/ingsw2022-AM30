c3cd087a9ace2b7406228d56348e01f764951afb
Daniele <daniele.asciutti@mail.polimi.it>
2022-05-27 16:49:49 +0200

    fix fxml

--- src/main/java/it/polimi/ingsw/client/View/gui/DashboardController.java
+++ src/main/java/it/polimi/ingsw/client/View/gui/DashboardController.java
@@ -1,8 +1,325 @@
 package it.polimi.ingsw.client.View.gui;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
 
 public class DashboardController {
+
+    @FXML
+    public Pane cloud1Pane;
+
+    @FXML
+    public ImageView cloud1Student2;
+
+    @FXML
+    public ImageView cloud1Student4;
+
+    @FXML
+    public ImageView cloud1Student3;
+
+    @FXML
+    public ImageView cloud1Student1;
+
+    @FXML
+    public Pane cloud3Pane;
+
+    @FXML
+    public ImageView cloud3Student2;
+
+    @FXML
+    public ImageView cloud3Student4;
+
+    @FXML
+    public ImageView cloud3Student3;
+
+    @FXML
+    public ImageView cloud3Student1;
+
+    @FXML
+    public Pane cloud2Pane;
+
+    @FXML
+    public ImageView cloud2Student2;
+
+    @FXML
+    public ImageView cloud2Student4;
+
+    @FXML
+    public ImageView cloud2Student3;
+
+    @FXML
+    public ImageView cloud2Student1;
+
+    @FXML
+    public Pane assistantsPane;
+
+    @FXML
+    public ImageView lion;
+
+    @FXML
+    public ImageView ostrich;
+
+    @FXML
+    public ImageView cat;
+
+    @FXML
+    public ImageView eagle;
+
+    @FXML
+    public ImageView fox;
+
+    @FXML
+    public ImageView snake;
+
+    @FXML
+    public ImageView octopus;
+
+    @FXML
+    public ImageView dog;
+
+    @FXML
+    public ImageView elephants;
+
+    @FXML
+    public ImageView turtle;
+
+    @FXML
+    public Pane plancePane;
+
+    @FXML
+    public ImageView entranceStudent9;
+
+    @FXML
+    public ImageView entranceStudent6;
+
+    @FXML
+    public ImageView entranceStudent4;
+
+    @FXML
+    public ImageView entranceStudent2;
+
+    @FXML
+    public ImageView entranceStudent1;
+
+    @FXML
+    public ImageView entranceStudent5;
+
+    @FXML
+    public ImageView entranceStudent3;
+
+    @FXML
+    public ImageView entranceStudent7;
+
+    @FXML
+    public ImageView entranceStudent8;
+
+    @FXML
+    public HBox hallGreen;
+
+    @FXML
+    public ImageView hall1Student1;
+
+    @FXML
+    public ImageView hall1Student2;
+
+    @FXML
+    public ImageView hall1Student3;
+
+    @FXML
+    public ImageView hall1Student4;
+
+    @FXML
+    public ImageView hall1Student5;
+
+    @FXML
+    public ImageView hall1Student6;
+
+    @FXML
+    public ImageView hall1Student7;
+
+    @FXML
+    public ImageView hall1Student8;
+
+    @FXML
+    public ImageView hall1Student9;
+
+    @FXML
+    public ImageView hall1Student10;
+
+    @FXML
+    public HBox hallRed;
+
+    @FXML
+    public ImageView hall2Student1;
+
+    @FXML
+    public ImageView hall2Student2;
+
+    @FXML
+    public ImageView hall2Student3;
+
+    @FXML
+    public ImageView hall2Student4;
+
+    @FXML
+    public ImageView hall2Student5;
+
+    @FXML
+    public ImageView hall2Student6;
+
+    @FXML
+    public ImageView hall2Student7;
+
+    @FXML
+    public ImageView hall2Student8;
+
+    @FXML
+    public ImageView hall2Student9;
+
+    @FXML
+    public ImageView hall2Student10;
+
+    @FXML
+    public HBox hallYellow;
+
+    @FXML
+    public ImageView hall3Student1;
+
+    @FXML
+    public ImageView hall3Student2;
+
+    @FXML
+    public ImageView hall3Student3;
+
+    @FXML
+    public ImageView hall3Student4;
+
+    @FXML
+    public ImageView hall3Student5;
+
+    @FXML
+    public ImageView hall3Student6;
+
+    @FXML
+    public ImageView hall3Student7;
+
+    @FXML
+    public ImageView hall3Student8;
+
+    @FXML
+    public ImageView hall3Student9;
+
+    @FXML
+    public ImageView hall3Student10;
+
+    @FXML
+    public HBox hallPink;
+
+    @FXML
+    public ImageView hall4Student1;
+
+    @FXML
+    public ImageView hall4Student2;
+
+    @FXML
+    public ImageView hall4Student3;
+
+    @FXML
+    public ImageView hall4Student4;
+
+    @FXML
+    public ImageView hall4Student5;
+
+    @FXML
+    public ImageView hall4Student6;
+
+    @FXML
+    public ImageView hall4Student7;
+
+    @FXML
+    public ImageView hall4Student8;
+
+    @FXML
+    public ImageView hall4Student9;
+
+    @FXML
+    public ImageView hall4Student10;
+
+    @FXML
+    public HBox hallBlue;
+
+    @FXML
+    public ImageView hall5Student1;
+
+    @FXML
+    public ImageView hall5Student2;
+
+    @FXML
+    public ImageView hall5Student3;
+
+    @FXML
+    public ImageView hall5Student4;
+
+    @FXML
+    public ImageView hall5Student5;
+
+    @FXML
+    public ImageView hall5Student6;
+
+    @FXML
+    public ImageView hall5Student7;
+
+    @FXML
+    public ImageView hall5Student8;
+
+    @FXML
+    public ImageView hall5Student9;
+
+    @FXML
+    public ImageView hall5Student10;
+
+    @FXML
+    public ImageView professorBlue;
+
+    @FXML
+    public ImageView professorGreen;
+
+    @FXML
+    public ImageView professorRed;
+
+    @FXML
+    public ImageView professorYellow;
+
+    @FXML
+    public ImageView professorPink;
+
+    @FXML
+    public Label nicknameLabel;
+
+    @FXML
+    public Label playerStateLabel;
+
+    @FXML
+    public Label gameUpdateLabel;
+
+    @FXML
+    public Pane characterButtor;
+
+    @FXML
+    public Label characterButtonLabel;
+
+    @FXML
+    public HBox coinPane;
+
+    @FXML
+    public Label coinLabel;
+
+    @FXML
+    public Pane charactersPane;
 
     public void chosenCloud1(MouseEvent mouseEvent) {
     }
