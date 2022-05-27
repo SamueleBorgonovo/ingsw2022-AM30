e01173e59b10bca6f406fb7695f6036d98dc0c9c
giuseppe <giuseppe.anzillotti@mail.polimi.it>
2022-05-27 16:29:53 +0200

    Added Eriantys Dashboard

---
+++ src/main/java/it/polimi/ingsw/client/View/gui/DashboardController.java
@@ -0,0 +1,93 @@
+package it.polimi.ingsw.client.View.gui;
+
+import javafx.scene.input.MouseEvent;
+
+public class DashboardController {
+
+    public void chosenCloud1(MouseEvent mouseEvent) {
+    }
+
+    public void chosenCloud3(MouseEvent mouseEvent) {
+    }
+
+    public void chosenCloud2(MouseEvent mouseEvent) {
+    }
+
+    public void chosenAssistantLion(MouseEvent mouseEvent) {
+    }
+
+    public void chosenAssistantOstrich(MouseEvent mouseEvent) {
+    }
+
+    public void chosenAssistantCat(MouseEvent mouseEvent) {
+    }
+
+    public void chosenAssistantEagle(MouseEvent mouseEvent) {
+    }
+
+    public void chosenAssistantFox(MouseEvent mouseEvent) {
+    }
+
+    public void chosenAssistantSnake(MouseEvent mouseEvent) {
+    }
+
+    public void chosenAssistantOctopus(MouseEvent mouseEvent) {
+    }
+
+    public void chosenAssistantDog(MouseEvent mouseEvent) {
+    }
+
+    public void chosenAssistantElephants(MouseEvent mouseEvent) {
+    }
+
+    public void chosenAssistantTurtle(MouseEvent mouseEvent) {
+    }
+
+    public void chosenEntranceStudent9(MouseEvent mouseEvent) {
+    }
+
+    public void chosenEntranceStudent6(MouseEvent mouseEvent) {
+    }
+
+    public void chosenEntranceStudent4(MouseEvent mouseEvent) {
+    }
+
+    public void chosenEntranceStudent2(MouseEvent mouseEvent) {
+    }
+
+    public void chosenEntranceStudent1(MouseEvent mouseEvent) {
+    }
+
+    public void chosenEntranceStudent5(MouseEvent mouseEvent) {
+    }
+
+    public void chosenEntranceStudent3(MouseEvent mouseEvent) {
+    }
+
+    public void chosenEntranceStudent7(MouseEvent mouseEvent) {
+    }
+
+    public void chosenEntranceStudent8(MouseEvent mouseEvent) {
+    }
+
+    public void chosenHallGreen(MouseEvent mouseEvent) {
+    }
+
+    public void chosenHallRed(MouseEvent mouseEvent) {
+    }
+
+    public void chosenHallYellow(MouseEvent mouseEvent) {
+    }
+
+    public void chosenHallPink(MouseEvent mouseEvent) {
+    }
+
+    public void chosenHallBlue(MouseEvent mouseEvent) {
+    }
+
+    public void clickCharacetrButton(MouseEvent mouseEvent) {
+    }
+
+    public void clickAssistabtButton(MouseEvent mouseEvent) {
+    }
+}
