61ceb073931e627de6a273578be363c3a21daec9
Daniele <daniele.asciutti@mail.polimi.it>
2022-06-17 22:29:13 +0200

    Add javadoc

--- src/main/java/it/polimi/ingsw/client/ClientMessageHandler.java
+++ src/main/java/it/polimi/ingsw/client/ClientMessageHandler.java
@@ -6,15 +6,28 @@ import it.polimi.ingsw.messages.toClient.InvalidMoveMessages.*;
 import it.polimi.ingsw.messages.toClient.StatesMessages.*;
 import it.polimi.ingsw.messages.toServer.PingToServerMessage;
 
+/**
+ * ClientMessageHandler handles all messages received from the server
+ */
 public class ClientMessageHandler {
 
     private Client client;
     private View view;
+
+    /**
+     * Constructor ClientMessageHandler instantiates client and view
+     * @param client client connected with the server
+     * @param view view of the client
+     */
     public ClientMessageHandler(Client client, View view){
         this.client=client;
         this.view=view;
     }
 
+    /**
+     * Method used to process PingToClientMessage
+     * @param message message received from the server
+     */
     public void process(PingToClientMessage message) {
         if (message.isPing()) {
             PingToServerMessage pong = new PingToServerMessage(false);
@@ -23,14 +36,26 @@ public class ClientMessageHandler {
         else client.stopTimer();
     }
 
+    /**
+     * Method used to process InvalidAssistantMessage
+     * @param message message received from the server
+     */
     public void process(InvalidAssistantMessage message){
         view.printInvalidAssistant();
     }
 
+    /**
+     * Method used to process InvalidTurnMessage
+     * @param message message received from the server
+     */
     public void process(InvalidTurnMessage message){
         view.printInvalidTurn();
     }
 
+    /**
+     * Method used to process InvalidStopMessage
+     * @param message message received from the server
+     */
     public void process(InvalidStopMessage message){
         view.printInvalidStop();
     }
@@ -43,6 +68,10 @@ public class ClientMessageHandler {
 
     }
 
+    /**
+     * Method used to process InvalidCloudMessage
+     * @param message message received from the server
+     */
     public void process(InvalidCloudMessage message){
         view.printInvalidCloud();
     }
@@ -51,6 +80,10 @@ public class ClientMessageHandler {
 
     }
 
+    /**
+     * Method used to process InvalidStudentMessage
+     * @param message message received from the server
+     */
     public void process(InvalidStudentMessage message){
         view.printInvalidStudent();
     }
@@ -59,10 +92,18 @@ public class ClientMessageHandler {
 
     }
 
+    /**
+     * Method used to process InvalidIslandMessage
+     * @param message message received from the server
+     */
     public void process(InvalidIslandMessage message){
         view.printInvalidIsland();
     }
 
+    /**
+     * Method used to process WizardsListMessage
+     * @param message message received from the server
+     */
     public void process(WizardsListMessage message){
         view.chooseWizard(message.getList());
     }
@@ -71,6 +112,10 @@ public class ClientMessageHandler {
 
     }
 
+    /**
+     * Method used to process DisconnectMessage
+     * @param message message received from the server
+     */
     public void process(DisconnectMessage message){
         client.handleDisconnection(message.getNickname(), message.isTimeout(),message.isWin());
     }
@@ -79,82 +124,162 @@ public class ClientMessageHandler {
 
     }
 
+    /**
+     * Method used to process InvalidWizardMessage
+     * @param message message received from the server
+     */
     public void process(InvalidWizardMessage message){
         view.printInvalidWizard();
     }
 
+    /**
+     * Method used to process PlayerStateMessage
+     * @param message message received from the server
+     */
     public void process(PlayerStateMessage message){
         view.nextMove(message.getState());
     }
 
+    /**
+     * Method used to process BoardUpdateMessage
+     * @param message message received from the server
+     */
     public void process(BoardUpdateMessage message){
         view.setBoard(message.getBoard());
     }
 
+    /**
+     * Method used to process SetTurnMessage
+     * @param message message received from the server
+     */
     public void process(SetTurnMessage message){
         client.setTurn(message.getNickname(), message.isassistantPhase());
     }
 
+    /**
+     * Method used to process StartGameMessage
+     * @param message message received from the server
+     */
     public void process(StartGameMessage message){
         view.printStartGame(message.isRestart());
     }
 
+    /**
+     * Method used to process NicknameMessage
+     * @param message message received from the server
+     */
     public void process(NicknameMessage message){
         view.chooseNickname(message.getCheck(),message.getReconnect());
     }
 
+    /**
+     * Method used to process ConnectMessage
+     * @param message message received from the server
+     */
     public void process(ConnectMessage message){
         view.printPlayerConnection(message.getNickname(),message.getReconnect());
     }
 
+    /**
+     * Method used to process PlanceUpdateMessage
+     * @param message message received from the server
+     */
     public void process(PlanceUpdateMessage message){
             view.setPlayers(message.getPlayers());
     }
 
+    /**
+     * Method used to process AssistantChoosedMessage
+     * @param message message received from the server
+     */
     public void process(AssistantChoosedMessage message){
         view.printAssistantChosen(message.getNickname(), message.getAssistant());
     }
 
+    /**
+     * Method used to process StudentHallChoosedMessage
+     * @param message message received from the server
+     */
     public void process(StudentHallChoosedMessage message){
         view.printStudentToHall(message.getNickname(),message.getStudent());
     }
 
+    /**
+     * Method used to process StudentIslandChoosedMessage
+     * @param message message received from the server
+     */
     public void process(StudentIslandChoosedMessage message){
         view.printStudentToIsland(message.getNickname(),message.getStudent(),message.getIslandID());
     }
 
+    /**
+     * Method used to process CharacterChoosedMessage
+     * @param message message received from the server
+     */
     public void process(CharacterChoosedMessage message){
         view.printCharacterChosen(message.getNickname(),message.getCharacter());
     }
 
+    /**
+     * Method used to process CloudChoosedMessage
+     * @param message message received from the server
+     */
     public void process(CloudChoosedMessage message){
         view.printCloudChosen(message.getNickname(),message.getCloud());
     }
 
+    /**
+     * Method used to process MotherNatureMoveMessage
+     * @param message message received from the server
+     */
     public void process(MotherNatureMoveMessage message){
         view.printMotherNatureMovement(message.getNickname(), message.getIslandId());
     }
 
+    /**
+     * Method used to process WinInstantlyMessage
+     * @param message message received from the server
+     */
     public void process(WinIstantlyMessage message){
         view.printWinnerInstantly(message.getNickname(),message.getWinType());
     }
 
+    /**
+     * Method used to process WinEndRoundMessage
+     * @param message message received from the server
+     */
     public void process(WinEndRoundMessage message){
         view.printWinnerEndRound(message.getNickname(),message.getWinType());
     }
 
+    /**
+     * Method used to process WaitingForPlayersMessage
+     * @param message message received from the server
+     */
     public void process(WaitingForPlayersMessage message){
         view.printWaitingForPlayers(message.isLobby());
     }
 
+    /**
+     * Method used to process EffectHandlerUpdateMessage
+     * @param message message received from the server
+     */
     public void process(EffectHandlerUpdateMessage message){
         view.setEffectHandler(message.getEffectHandler());
     }
 
+    /**
+     * Method used to process SettingReconnectMessage
+     * @param message message received from the server
+     */
     public void process(SettingReconnectMessage message){
         client.setSettings(message.getGameMode(),message.getNumOfPlayers(),message.getWizard());
     }
 
+    /**
+     * Method used to process CorrectlyConnectedMessage
+     * @param message message received from the server
+     */
     public void process(CorrectlyConnectedMessage message){
         view.correctlyConnected();
     }
--- src/main/java/it/polimi/ingsw/controller/MessageHandler.java
+++ src/main/java/it/polimi/ingsw/controller/MessageHandler.java
@@ -4,29 +4,60 @@ import it.polimi.ingsw.messages.toClient.PingToClientMessage;
 import it.polimi.ingsw.messages.toServer.*;
 import it.polimi.ingsw.server.ClientHandlerInterface;
 
+/**
+ * MessageHandler handles all messages received from the clients
+ */
 public class MessageHandler {
     GameHandler gameHandler;
 
+    /**
+     * Constructor messageHandler instantiates the gameHandler
+     */
     public MessageHandler(){
         gameHandler = new GameHandler();
     }
 
+    /**
+     * Method used to process ChooseAssistantMessage
+     * @param message message received from client
+     * @param clientHandler clientHandler of the client that sent the message
+     */
     public void process(ChooseAssistantMessage message, ClientHandlerInterface clientHandler){
         gameHandler.chooseAssistant(clientHandler,message.getAssistant());
     }
 
+    /**
+     * Method used to process ChooseCharacterMessage
+     * @param message message received from client
+     * @param clientHandler clientHandler of the client that sent the message
+     */
     public void process(ChooseCharacterMessage message, ClientHandlerInterface clientHandler){
         gameHandler.chooseCharacter(clientHandler,message.getCharacter());
     }
 
+    /**
+     * Method used to process ChooseCloudMessage
+     * @param message message received from client
+     * @param clientHandler clientHandler of the client that sent the message
+     */
     public void process(ChooseCloudMessage message, ClientHandlerInterface clientHandler){
         gameHandler.chooseCloud(clientHandler,message.getCloudID());
     }
 
+    /**
+     * Method used to process ChooseIslandEffectMessage
+     * @param message message received from client
+     * @param clientHandler clientHandler of the client that sent the message
+     */
     public void process(ChooseIslandEffectMessage message, ClientHandlerInterface clientHandler){
         gameHandler.chooseIslandEffect(clientHandler,message.getIslandID());
     }
 
+    /**
+     * Method used to process ChooseNicknameMessage
+     * @param message message received from client
+     * @param clientHandler clientHandler of the client that sent the message
+     */
     public void process(ChooseNicknameMessage message, ClientHandlerInterface clientHandler){
         if(message.getReconnect())
             gameHandler.reconnectPlayer(clientHandler);
@@ -34,30 +65,65 @@ public class MessageHandler {
             gameHandler.checkNickname(clientHandler,message.getNickname(),message.getNewGame());
     }
 
+    /**
+     * Method used to process ChooseStudentsEffectMessage
+     * @param message message received from client
+     * @param clientHandler clientHandler of the client that sent the message
+     */
     public void process(ChooseStudentsEffectMessage message, ClientHandlerInterface clientHandler){
         gameHandler.chooseStudentsEffect(clientHandler, message.getStudents());
     }
 
+    /**
+     * Method used to process ChooseWizardMessage
+     * @param message message received from client
+     * @param clientHandler clientHandler of the client that sent the message
+     */
     public void process(ChooseWizardMessage message, ClientHandlerInterface clientHandler){
         gameHandler.chooseWizard(clientHandler,message.getWizard());
     }
 
+    /**
+     * Method used to process CreatePlayerInGameMessage
+     * @param message message received from client
+     * @param clientHandler clientHandler of the client that sent the message
+     */
     public void process(CreatePlayerInGameMessage message, ClientHandlerInterface clientHandler){
         gameHandler.addPlayer(clientHandler, message.getGamemode(), message.getNumofplayers());
     }
 
+    /**
+     * Method used to process MoveMotherNatureMessage
+     * @param message message received from client
+     * @param clientHandler clientHandler of the client that sent the message
+     */
     public void process(MoveMotherNatureMessage message, ClientHandlerInterface clientHandler){
         gameHandler.moveMotherNature(clientHandler, message.getMovement());
     }
 
+    /**
+     * Method used to process MoveStudentToHallMessage
+     * @param message message received from client
+     * @param clientHandler clientHandler of the client that sent the message
+     */
     public void process(MoveStudentToHallMessage message, ClientHandlerInterface clientHandler){
         gameHandler.moveStudentToHall(clientHandler, message.getStudent());
     }
 
+    /**
+     * Method used to process MoveStudentToIslandMessage
+     * @param message message received from client
+     * @param clientHandler clientHandler of the client that sent the message
+     */
     public void process(MoveStudentToIslandMessage message, ClientHandlerInterface clientHandler){
         gameHandler.moveStudentToIsland(clientHandler, message.getIslandID(), message.getStudent());
     }
 
+    /**
+     * Method used to process PingToServerMessage
+     * @param message message received from client
+     * @param clientHandler clientHandler of the client that sent the message
+     */
     public void process(PingToServerMessage message, ClientHandlerInterface clientHandler) {
         if (message.isPing()) {
             PingToClientMessage message2 = new PingToClientMessage(false);
@@ -67,6 +133,11 @@ public class MessageHandler {
         }
     }
 
+    /**
+     * Method used to process DisconnectionRespondMessage
+     * @param message message received from client
+     * @param clientHandler clientHandler of the client that sent the message
+     */
     public void process(DisconnectionRespondMessage message,ClientHandlerInterface clientHandler){
         clientHandler.handleSocketDisconnection(false,true);
     }
