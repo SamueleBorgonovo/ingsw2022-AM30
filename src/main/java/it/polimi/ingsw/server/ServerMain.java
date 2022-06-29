44947ee035feecaf26e22d75ad2a4f14465e3c17
Daniele <daniele.asciutti@mail.polimi.it>
2022-06-29 10:59:51 +0200

    fix server ip issue

--- src/main/java/it/polimi/ingsw/server/ServerMain.java
+++ src/main/java/it/polimi/ingsw/server/ServerMain.java
@@ -1,14 +1,8 @@
 package it.polimi.ingsw.server;
 
 import it.polimi.ingsw.client.View.cli.InputParser;
-
 import java.io.IOException;
-import java.lang.reflect.Array;
 import java.net.*;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Enumeration;
-import java.util.NoSuchElementException;
 
 /**
  * Class used to start the server on a given port
@@ -27,53 +21,7 @@ public class ServerMain {
             System.out.println("Port Number is not valid, please insert a new one");
             serverPort=inputParser.intParser();
         }
-        int i=0;
-    /*   try {
-            System.out.println(System.getProperty("os.name"));
-            Enumeration networkInterfaces = NetworkInterface.getNetworkInterfaces();
-            while (networkInterfaces.hasMoreElements()) {
-                NetworkInterface inet = (NetworkInterface) networkInterfaces.nextElement();
-                Enumeration address = inet.getInetAddresses();
-                while (address.hasMoreElements()) {
-                    InetAddress inetAddress = (InetAddress) address.nextElement();
-                    if (inetAddress.isSiteLocalAddress()) {
-                        if (i == 0) {
-                            System.out.println("LAN ip: " + inetAddress.getHostAddress());
-                            i++;
-                        }else System.out.println("Wi-Fi ip: "+inetAddress.getHostAddress());
-                    }
-                }
-            }
-        } catch (Exception ignored) {}
-
-     */
-        String[] array = new String[1];
-        array[0]="/192";
-            Enumeration<NetworkInterface> nets = NetworkInterface.getNetworkInterfaces();
-            for(NetworkInterface net: Collections.list(nets)){
-                try {
-                    NetworkInterface nif = net;
-
-                 Enumeration<InetAddress> nifAddresses = nif.getInetAddresses();
-                 InetSocketAddress inetAddr = new InetSocketAddress(nifAddresses.nextElement(), 0);
-                    DatagramSocket socket = new DatagramSocket(inetAddr);
-
-                 //if(Arrays.stream(array).anyMatch(socket.getLocalAddress().toString()::contains)) {
-                     //if(Arrays.stream(new String[]{"eth"}).anyMatch(nif.getName()::contains))
-                    System.out.println(nif.getName());
-                    System.out.println(socket.getInetAddress());
-                        System.out.println("LAN: " + socket.getLocalAddress());
-                     //if(Arrays.stream(new String[]{"wlan"}).anyMatch(nif.getName()::contains))
-                        System.out.println("Wi-Fi: " + socket.getLocalAddress());
-                 //}
-              } catch(SocketException ex){
-                    System.out.println(ex.toString());
-              }
-              catch(NoSuchElementException ex)
-              {
-                    //System.out.println(ex.toString());
-              }
-        }
+        System.out.println("Server ip: "+Inet4Address.getLocalHost().getHostAddress());
         System.out.println("Server listening on port: " + serverPort);
         Server server = new Server(serverPort);
         server.start();
