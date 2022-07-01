5aba8a1316e980db8683f618f974c05e7f546fd6
samub <samuele.borgonovo@mail.polimi.it>
2022-07-01 22:16:41 +0200

    final jar

--- src/main/java/it/polimi/ingsw/client/View/cli/CLI.java
+++ src/main/java/it/polimi/ingsw/client/View/cli/CLI.java
@@ -41,8 +41,8 @@ public class CLI implements View {
     /**
      * Method init starts a new cli instance
      *
-     * @throws IOException
-     * @throws NumberFormatException
+     * @throws IOException signals that an I/O exception of some sort has occurred
+     * @throws NumberFormatException Thrown to indicate that the application has attempted to convert a string to one of the numeric types, but that the string does not have the appropriate format
      */
     public void init() throws IOException, NumberFormatException {
         boolean check = false;
--- src/main/java/it/polimi/ingsw/model/player/Assistant.java
+++ src/main/java/it/polimi/ingsw/model/player/Assistant.java
@@ -18,19 +18,39 @@ public enum Assistant {
     private final int value;
     private final int movement;
 
+    /**
+     * Constructor Assistant creates a new assistant card
+     * @param value the value of the assistant
+     * @param movement the movement possible by the assistant
+     */
     Assistant(int value, int movement){
         this.value = value;
         this.movement = movement;
     }
 
+    /**
+     * Method getValue returns the value of the assistant
+     *
+     * @return the value of the assistant
+     */
     public int getValue(){
         return value;
     }
 
+    /**
+     * Method getMovement return the movement of the assistant
+     *
+     * @return the movement of the assistant
+     */
     public int getMovement(){
         return movement;
     }
 
+    /** Method getText returns the name of the assistant
+     *
+     * @param assistant the assistant requested
+     * @return the name of the assistant
+     */
     public String getText(Assistant assistant){
         return assistant.toString().toLowerCase();
     }
